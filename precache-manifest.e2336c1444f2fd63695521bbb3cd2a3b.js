d202b9d3ef91ffaee994b376ed7e4bc380fb18ef
Dexter.Penafiel <Dexter.Penafiel@ertechnologies.com>
2019-05-31 18:14:44 +0800

    Updates

---
+++ precache-manifest.e2336c1444f2fd63695521bbb3cd2a3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd74ca66bf26df501c5db7df3993b8e4",
+    "url": "/mous-opt/index.html"
+  },
+  {
+    "revision": "dd63c7d5cad82d127084",
+    "url": "/mous-opt/static/css/2.a2da144b.chunk.css"
+  },
+  {
+    "revision": "c1189f59b3d5aecca9f9",
+    "url": "/mous-opt/static/css/main.57c00206.chunk.css"
+  },
+  {
+    "revision": "dd63c7d5cad82d127084",
+    "url": "/mous-opt/static/js/2.16068a75.chunk.js"
+  },
+  {
+    "revision": "c1189f59b3d5aecca9f9",
+    "url": "/mous-opt/static/js/main.2dd75de4.chunk.js"
+  },
+  {
+    "revision": "09a001a2966ff73c110d",
+    "url": "/mous-opt/static/js/runtime~main.64a8bc37.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mous-opt/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mous-opt/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mous-opt/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mous-opt/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mous-opt/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
